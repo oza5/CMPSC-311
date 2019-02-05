5e02d367e192b1880334104cb0236b0979bd07a6
Osman Ali <44344667+oza5@users.noreply.github.com>
2019-02-04 19:37:34 -0500

    Doubly Linked List
    Doubly Linked List

--- hw8handout/doublylinkedlist.c
+++ hw8handout/doublylinkedlist.c
@@ -1,19 +1,18 @@
 /* Yanling Wang Fall 2018 */
-//OSMAN ALI
 #include <stdio.h>
 #include <stdlib.h>
 #include "doublylinkedlist.h"
 
 void insertHead(doublyLinkedList *listPtr, int value) {
   listNode *ins_h = (listNode*)malloc(sizeof(listNode));
-  if (listPtr->head == NULL || listPtr->tail == NULL){  //empty list
+  if (listPtr->head == NULL || listPtr->tail == NULL){  //empty list 
 	ins_h->value = value;
 	ins_h->next = NULL;
         ins_h->prev = NULL;
 	listPtr->head = ins_h;
 	listPtr->tail = ins_h;
   }
-
+	
   else{							//Non-empty lsit
 	listNode *temp = listPtr->head;
         ins_h->value = value;
@@ -21,7 +20,7 @@ void insertHead(doublyLinkedList *listPtr, int value) {
 	ins_h->prev = NULL;
 	temp->prev = ins_h;
 	listPtr->head = ins_h;
-  }
+  } 
   return;
 }
 
@@ -38,7 +37,7 @@ int removeTail(doublyLinkedList *listPtr) {
 	free(temp);
   }
 
-  else{                                                 //more than one element
+  else{                                                 //more than on element
 	listNode *temp = listPtr->tail;
 	temp->prev->next = NULL;
         listPtr->tail = temp->prev;
@@ -46,7 +45,7 @@ int removeTail(doublyLinkedList *listPtr) {
   }
 
   return c;
-}
+} 
 
 void showHead(doublyLinkedList *listPtr) {
   listNode *head = listPtr->head;
@@ -54,21 +53,21 @@ void showHead(doublyLinkedList *listPtr) {
   while (head != NULL) {
     printf("%d ", head->value);
     head = head->next;
-  }
+  } 
   printf("nil\n");
   return;
-}
+} 
 void showTail(doublyLinkedList *listPtr) {
   listNode *tail = listPtr->tail;
   printf("list tail: ");
   while (tail != NULL) {
     printf("%d ", tail->value);
-    tail = tail->prev;
-  }
+    tail = tail->prev; 
+  } 
   printf("nil\n");
   return;
-}
-void freeList(doublyLinkedList *listPtr) {
+} 
+void freeList(doublyLinkedList *listPtr) { 
   listNode *head = listPtr->head;
   listNode *temp;
   while(head != NULL) {
@@ -80,3 +79,4 @@ void freeList(doublyLinkedList *listPtr) {
   listPtr->tail = NULL;
   return;
 }
+
